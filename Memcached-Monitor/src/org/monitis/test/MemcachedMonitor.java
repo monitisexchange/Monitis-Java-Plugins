2bac323ce5fd63ede44a2e45989c83322b91035a
simon hunanyan <simon.hunanyan@gmail.com>
2012-04-17 15:46:55 +0400

    Changes in JAva Memcached Monitor

--- Memcached-Monitor/src/org/monitis/test/MemcachedMonitor.java
+++ Memcached-Monitor/src/org/monitis/test/MemcachedMonitor.java
@@ -1,7 +1,5 @@
 package org.monitis.test;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
 import java.net.InetSocketAddress;
 import java.net.SocketAddress;
 import java.util.ArrayList;
@@ -16,32 +14,29 @@ import net.rubyeye.xmemcached.XMemcachedClientBuilder;
 import net.rubyeye.xmemcached.command.BinaryCommandFactory;
 import net.rubyeye.xmemcached.utils.AddrUtil;
 
+import org.json.JSONArray;
 import org.monitis.GenericCustomMonitor.GenericCustomMonitorRunner;
-import org.monitis.GenericCustomMonitor.IGenericCustomMonitor;
+import org.monitis.GenericCustomMonitor.IGenericCustomMonitorWrapper;
 import org.monitis.beans.MonResult;
-import org.monitis.beans.MonResultParameter;
-import org.monitis.beans.MonitorParameter;
-import org.monitis.enums.DataType;
+import org.monitis.utils.MConfig;
 import org.monitis.utils.TimeUtility;
+import org.monitis.utils.Utils;
 
-public class MemcachedMonitor extends  IGenericCustomMonitor {
-	
-	private static final String apiKey = "2PE0HVI4DHP34JACKCAE37IOD4";		// <- replace by your API key (can be get from your Monitis account)
-	private static final String secretKey = "7OI90FU3C3DA8ENLNJ0JGGOGO0";	// <- replace by your Secure key (can be get from your Monitis account)
-    private static final String monitor_name = "Custom_monitor";			// <- replace by your desired monitor name
-    private static final String monitor_tag_value = "Memcached_monitor";	// <- replace by your desired monitor Group
-    private static String servers = "localhost:11211";						// <- replace by your monitored Memcached server connection parameters
-    private static final long processingTime = 60;							//default value - 60 sec (1 min)
-    private static final long testDuration = 600;							//default value - 600 sec (10 min)
-    private static Map<InetSocketAddress,Map<String,String>> settings = null;
-
+public class MemcachedMonitor extends  IGenericCustomMonitorWrapper {
 	
     MemcachedClient cache = null;
+	private static String servers;
+	private static Map<InetSocketAddress,Map<String,String>> settings = null;
+	private static String additionalResult = "details";
+	private static List<String> status = null;
     //storage of previous measured statistics
     Map<InetSocketAddress,Map<String,String>> stats_ = new HashMap<InetSocketAddress, Map<String, String>>();
     
     public MemcachedMonitor () throws Exception {
+    	MConfig conf = MConfig.getConfig("/properties/api_config.json");
+    	servers = conf.getConfigStringValue("monitor.memcached_server", "localhost:11211");
 		cache = buildMemcachedClient(servers);
+		System.out.println("cache = "+cache);
     }
 
     private MemcachedClient buildMemcachedClient(String servers) throws Exception {
@@ -49,55 +44,8 @@ public class MemcachedMonitor extends  IGenericCustomMonitor {
 		builder.setCommandFactory(new BinaryCommandFactory());// use binary protocol
 		return builder.build();
     }
-	
-	public String get_apiKey() {
-		return apiKey;
-	}
-	
-	public String get_secretKey() {
-		return secretKey;
-	}
-	
-	public String get_monitor_name() {
-		return monitor_name;
-	}
-	
-	public String get_monitor_tag_value() {
-		return monitor_tag_value;
-	}
 
-	public long get_processingTime() {
-		return processingTime * 1000;//ms
-
-	}
-
-	public long get_testDuration() {
-		return testDuration * 1000;//ms
-	}
-	
-	public boolean deleteMonitor() {
-		return false;
-	}
-	
-	public List<MonitorParameter> get_monitorParams() {
-		return null;
-	}
-
-	public List<MonResultParameter> get_resultParams() {
-		List<MonResultParameter> resultParams = new ArrayList<MonResultParameter>();
-		resultParams.add(new MonResultParameter("server", "server", "", DataType.STRING));
-		resultParams.add(new MonResultParameter("conn", "conns [%]", "%", DataType.FLOAT));
-		resultParams.add(new MonResultParameter("get_miss", "get_miss [%]", "%", DataType.FLOAT));
-		resultParams.add(new MonResultParameter("delete_miss", "delete_miss [%]", "%", DataType.FLOAT));
-		resultParams.add(new MonResultParameter("incr_miss", "incr_miss [%]", "%", DataType.FLOAT));
-		resultParams.add(new MonResultParameter("decr_miss", "decr_miss [%]", "%", DataType.FLOAT));
-		resultParams.add(new MonResultParameter("mem_usage", "mem_usage [%]", "%", DataType.FLOAT));
-		resultParams.add(new MonResultParameter("evictions", "evictions [%]", "%", DataType.FLOAT));
-		resultParams.add(new MonResultParameter("reqs", "reqs [per/s]", "", DataType.INTEGER));
-		
-		return resultParams;
-	}
-
+    @Override
 	public List<MonResult> get_results() {
 		Map<InetSocketAddress,Map<String,String>> stats = null;
 		int curr_connections, curr_connections_, maxconns;
@@ -114,10 +62,13 @@ public class MemcachedMonitor extends  IGenericCustomMonitor {
 		long evictions, evictions_;
 		long bytes, bytes_;
 //		int threads, threads_;
-//		long bytes_read, bytes_read_;
-//		long bytes_written, bytes_written_;
+		long bytes_read, bytes_read_;
+		long bytes_written, bytes_written_;
+		long uptime;
 		long lres;
+		long duration = get_processingTime()/1000; //[sec]
 		String res;
+		double dres; 
 		
 		try {
 			if (settings == null) {// Get settings
@@ -131,15 +82,17 @@ public class MemcachedMonitor extends  IGenericCustomMonitor {
         Iterator<InetSocketAddress> it = stats.keySet().iterator();
         StringBuffer buf = new StringBuffer();
 		List<MonResult> results = new ArrayList<MonResult>();
+		status = new ArrayList<String>();
 		
         while (it.hasNext()) {
             SocketAddress s = it.next();
-			results.add(new MonResult("server", s.toString()));
+//			results.add(new MonResult("server", s.toString()));
             buf.append("\n---- Statistics for server ").append(s.toString()).append("\n");
             buf.append(TimeUtility.format(TimeUtility.getNowByGMT().getTime(), "yyyy-MM-dd HH:mm:ss"));
 
 			//Current results
             Map<String, String> stat = stats.get(s);
+            uptime = Long.parseLong(stat.get("uptime"));//
 			curr_connections = Integer.parseInt(stat.get("curr_connections")); /*Number of open connections*/
 			maxconns = Integer.parseInt(settings.get(s).get("maxconns")); /*Number of max connections*/
 			get_hits = Long.parseLong(stat.get("get_hits")); /*Number of keys that have been requested and found present*/
@@ -154,14 +107,14 @@ public class MemcachedMonitor extends  IGenericCustomMonitor {
 			bytes = Long.parseLong(stat.get("bytes")); /*Current number of bytes used to store items*/
 			curr_items = Integer.parseInt(stat.get("curr_items")); /*Current number of items stored*/
 			evictions = Long.parseLong(stat.get("evictions"));/*Number of valid items removed from cache to free memory for new items*/
-//			bytes_read = Long.parseLong(stat.get("bytes_read"));/*Total number of bytes read by this server from network*/
-//			bytes_written = Long.parseLong(stat.get("bytes_written"));/*Total number of bytes sent by this server to network*/
+			bytes_read = Long.parseLong(stat.get("bytes_read"));/*Total number of bytes read by this server from network*/
+			bytes_written = Long.parseLong(stat.get("bytes_written"));/*Total number of bytes sent by this server to network*/
 //			threads = Integer.parseInt(stat.get("threads")); /*Number of worker threads requested*/
 			
 			//previous results
             if (stats_.size() <= 0) {
 				curr_connections_ = /*threads_ =*/ curr_items_ = 0;
-				get_hits_ = get_misses_ = limit_maxbytes_ = bytes_ = /*bytes_read_ = bytes_written_ =*/ evictions_ = 0;
+				get_hits_ = get_misses_ = limit_maxbytes_ = bytes_ = bytes_read_ = bytes_written_ = evictions_ = 0;
             } else {
                 Map<String, String> stat_ = stats_.get(s);
     			curr_connections_ = Integer.parseInt(stat_.get("curr_connections")); /*Number of open connections*/
@@ -177,117 +130,127 @@ public class MemcachedMonitor extends  IGenericCustomMonitor {
     			bytes_ = Long.parseLong(stat_.get("bytes")); /*Current number of bytes used to store items*/
     			curr_items_ = Integer.parseInt(stat_.get("curr_items")); /*Current number of items stored*/
     			evictions_ = Long.parseLong(stat_.get("evictions"));/*Number of valid items removed from cache to free memory for new items*/
-//    			bytes_read_ = Long.parseLong(stat_.get("bytes_read"));/*Total number of bytes read by this server from network*/
-//    			bytes_written_ = Long.parseLong(stat_.get("bytes_written"));/*Total number of bytes sent by this server to network*/
+    			bytes_read_ = Long.parseLong(stat_.get("bytes_read"));/*Total number of bytes read by this server from network*/
+    			bytes_written_ = Long.parseLong(stat_.get("bytes_written"));/*Total number of bytes sent by this server to network*/
 //    			threads_ = Integer.parseInt(stat_.get("threads")); /*Number of worker threads requested*/
             }
             
 // 			Percent of open connections to max connections
-			res = String.format("%3.1f", (curr_connections * 100.0) / maxconns);
+            dres = (curr_connections * 100.0) / maxconns;
+			res = String.format("%3.1f", dres);
 			results.add(new MonResult("conn", res));
+			if (dres > 95){
+				status.add(additionalResult+":"+"WARN - The number of connections reached to maximum allowed");
+			}
 			buf.append("\tconn: ").append(res);
  			
 //			Percent of items that have been requested and not found to total number of get commands 
 			lres = (get_hits + get_misses);
-			res = String.format("%3.1f", ((get_misses - get_misses_) * 100.0) / (lres<1?1:lres));
+			dres = ((get_misses - get_misses_) * 100.0) / (lres<1?1:lres);
+			res = String.format("%3.1f", dres);
 			results.add(new MonResult("get_miss", res));
+			if (dres > 5){
+				status.add(additionalResult+":"+"WARN - too many requested to get but non-found (lost) records");
+			}
 			buf.append("\tget_miss: ").append(res);
 			
 //			Percent of items that have been requested to delete and not found to total number of delete commands
 			lres = (delete_hits + delete_misses);
-			res = String.format("%3.1f", (delete_misses * 100.0) / (lres<1?1:lres));
+			dres = (delete_misses * 100.0) / (lres<1?1:lres);
+			res = String.format("%3.1f", dres);
 			results.add(new MonResult("delete_miss", res));
+			if (dres > 10){
+				status.add(additionalResult+":"+"WARN - too many requested to delete but already deleted records");
+			}
 			buf.append("\tdelete_miss: ").append(res);
 
 //			Percent of items that have been requested to increase and not found to total number of increase commands
 			lres = (incr_hits + incr_misses);
-			res = String.format("%3.1f", (incr_misses * 100.0) / (lres<1?1:lres));
+			dres = (incr_misses * 100.0) / (lres<1?1:lres);
+			res = String.format("%3.1f", dres);
 			results.add(new MonResult("incr_miss", res));
+			if (dres > 5){
+				status.add(additionalResult+":"+"WARN - too many requested to increase but non-found (lost) records");
+			}			
 			buf.append("\tincr_miss: ").append(res);
 			
 //			Percent of items that have been requested to decrease and not found to total number of decrease commands
 			lres = (decr_hits + decr_misses);
-			res = String.format("%3.1f", (decr_misses * 100.0) / (lres<1?1:lres));
+			dres = (decr_misses * 100.0) / (lres<1?1:lres);
+			res = String.format("%3.1f", dres);
 			results.add(new MonResult("decr_miss", res));
+			if (dres > 5){
+				status.add(additionalResult+":"+"WARN - too many requested to decrease but non-found (lost) records");
+			}			
 			buf.append("\tdecr_miss: ").append(res);
 			
 //			Percent of current number of bytes used to store items to the max accessible bytes
-			res = String.format("%3.1f", (bytes * 100.0) / limit_maxbytes);
+			dres = (bytes * 100.0) / limit_maxbytes;
+			res = String.format("%3.1f", dres);
 			results.add(new MonResult("mem_usage", res));
+			if (dres > 95){
+				status.add(additionalResult+":"+"WARN - the memory usage reached to critical point");
+			}			
 			buf.append("\tmem_usage: ").append(res);
 			
 //			Percent of valid items removed from cache to free memory to current number of items stored
-			res = String.format("%3.1f", (1.0 - (evictions / (curr_items < 1?1:curr_items))) * 100.0);
+			dres = (100.0 *(evictions - evictions_) / (curr_items < 1?1:curr_items));
+			res = String.format("%3.1f", dres);
 			results.add(new MonResult("evictions", res));
+			if (dres > 5){
+				status.add(additionalResult+":"+"WARN - Perhaps, the memory limit is to small");
+			}			
 			buf.append("\tevictions: ").append(res);
 			
- 			//Total number of keys that have been requested per sec.
-			res = String.valueOf(((get_hits +  get_misses)-(get_hits_ +  get_misses_)) / processingTime);
+// 			Total number of keys that have been requested per sec.
+			res = String.valueOf(((get_hits +  get_misses)-(get_hits_ +  get_misses_)) / duration);
 			results.add(new MonResult("reqs", res));
 			buf.append("\treqs: ").append(res);
 			
-//          //Number of open connections
-//			results.add(new MonResult("conns", String.valueOf(curr_connections)));
-//			buf.append("\tconns: ").append(String.valueOf(curr_connections));
-//						
-//			//Number of keys that have been requested and found present per sec.
-//			res = String.valueOf((get_hits - get_hits_) / processingTime);
-//			results.add(new MonResult("get_hits", res));
-// 			buf.append("\tget_hits: ").append(res);
-//
-//			//Number of items that have been requested and not found per sec.
-//			res = String.valueOf((get_misses - get_misses_) / processingTime);
-//			results.add(new MonResult("get_misses", res));
-// 			buf.append("\tget_misses: ").append(res);
-// 			
-//			//Percent of number of keys that have been requested and found present to total number of keys that have been requested
-//			res = String.format("%3.1f", (get_hits < 1?1: get_hits)  * 100.0 / ((get_hits +  get_misses) < 1? 1:(get_hits +  get_misses)));
-//			results.add(new MonResult("hitratio", res));
-//			buf.append("\thitratio: ").append(res);
-//
-//			//Current number of bytes used to store items [MB]
-//			res = String.format("%5.1f", bytes / 1024.0/1024.0);
-//			results.add(new MonResult("bytes", res));
-// 			buf.append("\tbytes: ").append(res);
-// 			
-//			//Current number of items stored
-//			results.add(new MonResult("curr_items", String.valueOf(curr_items)));
-//			buf.append("\tcurr_items: ").append(String.valueOf(curr_items));
-// 
-//			//Total number of bytes read by this server from network per sec.
-//			res = String.valueOf((bytes_read - bytes_read_) / processingTime);
-//			results.add(new MonResult("bytes_read", res));
-// 			buf.append("\tbytes_read: ").append(res);
-// 			
-// 			//Total number of bytes sent by this server to network per sec.
-//			res = String.valueOf((bytes_written - bytes_written_) / processingTime);
-//			results.add(new MonResult("bytes_written", res));
-// 			buf.append("\tbytes_written: ").append(res);
-// 			
-// 			// Percent of valid items removed from cache to free memory to current number of items stored
-//			res = String.format("%3.1f", evictions * 100.0 / (curr_items < 1?1:curr_items));
-//			results.add(new MonResult("evictions", res));
-//			buf.append("\tevictions: ").append(res);
-//			
-//			//Number of worker threads requested
-//			results.add(new MonResult("threads", String.valueOf(threads)));
-//			buf.append("\tthreads: ").append(String.valueOf(threads));
- 
+// 			outbound written KB per sec
+			res = String.valueOf((bytes_written - bytes_written_) / duration / 1000);
+			results.add(new MonResult("out_kbps", res));
+			buf.append("\tout_kbps: ").append(res);
+			
+// 			inbound written KB per sec
+			res = String.valueOf((bytes_read - bytes_read_) / duration / 1000);
+			results.add(new MonResult("in_kbps", res));
+			buf.append("\tin_kbps: ").append(res);
+
+// 			Memcached Uptime
+			res = Utils.formatTimestamp(uptime);
+			results.add(new MonResult("uptime", res));
+			
+			if (status == null || status.size() <= 0){
+				results.add(new MonResult("status", "OK"));
+			} else {
+				results.add(new MonResult("status", "NOK"));				
+			}
         }
         System.out.println(buf.toString());
         stats_.putAll(stats);
         
-		try {
-			String filename = "./ttt.txt";
-			BufferedWriter out = new BufferedWriter(new FileWriter(filename, true));
-			out.write(buf.toString());
-			out.close();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+//		try {
+//			String filename = "./ttt.txt";
+//			BufferedWriter out = new BufferedWriter(new FileWriter(filename, true));
+//			out.write(buf.toString());
+//			out.close();
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
 		
 		return results;
 	}
+
+	@Override
+	public JSONArray get_additionalResults() {
+		JSONArray jarr = null;
+		if (status != null && status.size() > 0){
+			jarr = new JSONArray(status);
+		} 
+		return jarr;
+	}
+
 
 	public static void main(String[] args) {
 		try {
